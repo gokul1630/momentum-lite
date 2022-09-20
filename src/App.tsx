8ab685212dc1131703e581b4b591a530916e57b1
gokul1630 <gokulgp3167@gmail.com>
2022-09-20 16:29:20 +0530

    Initial Momentum-lite

---
+++ src/App.tsx
@@ -0,0 +1,174 @@
+import moment from 'moment'
+import { useEffect, useLayoutEffect, useState } from 'react'
+import background from './assets/background.webp'
+import edit from './assets/edit.svg'
+import settings from './assets/settings.svg'
+import eye from './assets/eye.svg'
+import eyeOff from './assets/eyeOff.svg'
+import { Form } from './Form'
+
+const TopSiteList = ({ sites, slices, className }) => {
+	return sites?.slice(0, slices).map(({ title, url }) => {
+		return (
+			<li key={url} className={className}>
+				<a className='text-white flex flex-col items-center' href={url}>
+					<div className='bg-slate-300 max-w-[3.5rem] max-h-[3.5rem] w-full h-full p-5 rounded-full flex justify-center items-center text-gray-600 capitalize text-2xl'>
+						{title.match(/[A-Za-z]/)}
+					</div>
+					<h3 className='max-w-[6rem] mt-2 text-lg text-ellipsis overflow-hidden whitespace-nowrap'>
+						{title}
+					</h3>
+				</a>
+			</li>
+		)
+	})
+}
+
+const App = () => {
+	const [inputValue, setInputValue] = useState('')
+	const [name, setName] = useState('')
+	const [toggleTopSite, setToggleTopSite] = useState(true)
+	const [isEditName, setIsEditName] = useState(false)
+	const [{ hours, minutes }, setTime] = useState({ hours: '', minutes: '' })
+	const [sites, setSites] = useState([])
+
+	const formSubmit = () => {
+		window.location.reload()
+		localStorage.setItem('userName', inputValue)
+		setIsEditName(false)
+	}
+	const inputHandler = (event: any) => {
+		const { value } = event.target
+		setInputValue(value)
+	}
+
+	const onEditName = () => {
+		setIsEditName((state) => !state)
+		setInputValue('')
+	}
+
+	const handleUserName = (userName: string) => {
+		setName(userName)
+	}
+
+	useLayoutEffect(() => {
+		const time = moment()
+		const userName = localStorage.getItem('userName')
+		const currentHour = parseInt(time.format('HH'))
+
+		if (userName) {
+			if (currentHour >= 3 && currentHour < 12) {
+				handleUserName(`Good Morning ${userName}.`)
+			} else if (currentHour >= 12 && currentHour < 15) {
+				handleUserName(`Good Afternoon ${userName}.`)
+			} else if (currentHour >= 15 && currentHour < 20) {
+				handleUserName(`Good Evening ${userName}.`)
+			} else if (currentHour >= 20 && currentHour < 3) {
+				handleUserName(`Good Night ${userName}.`)
+			} else {
+				handleUserName(`Hello ${userName}.`)
+			}
+		}
+	}, [isEditName, name, inputValue])
+
+	const timeHandler = () => {
+		const time = moment()
+		setTime({ hours: time.format('h'), minutes: time.format('m a') })
+	}
+	const toggleTopSites = () => {
+		setToggleTopSite((state) => !state)
+		if (toggleTopSite) {
+			localStorage.setItem('topSites', '0')
+		} else {
+			localStorage.setItem('topSites', '1')
+		}
+	}
+	useEffect(() => {
+		setInterval(timeHandler, 1000)
+	}, [])
+
+	useLayoutEffect(() => {
+		timeHandler()
+		const showTopSites = parseInt(localStorage.getItem('topSites'))
+		if (showTopSites !== 1) {
+			setToggleTopSite(false)
+		}
+		// @ts-ignore
+		chrome.topSites
+			.get()
+			.then((response) => setSites(response))
+			.catch((err) => console.log('error', err))
+	}, [])
+
+	return (
+		<>
+			<img
+				src={background}
+				alt='background image'
+				className='absolute w-screen h-screen inset-0 overflow-hidden object-cover -z-10'
+			/>
+			<div className='flex justify-center items-center h-screen w-screen select-none bg-black/20 relative'>
+				{!name && !isEditName && (
+					<Form
+						onCancel={onEditName}
+						name={name}
+						inputValue={inputValue}
+						inputHandler={inputHandler}
+						onSubmit={formSubmit}
+					/>
+				)}
+				{isEditName && (
+					<Form
+						name={name}
+						onCancel={onEditName}
+						inputValue={inputValue}
+						inputHandler={inputHandler}
+						onSubmit={formSubmit}
+					/>
+				)}
+				{name && !isEditName && (
+					<div className='flex flex-col justify-center items-center'>
+						{hours && minutes && (
+							<h1 className='text-6xl lg:text-8xl font-medium text-white z-10 cursor-default select-none'>
+								<span className='text-7xl lg:text-9xl font-bold'>{hours}</span>:{minutes}
+							</h1>
+						)}
+						<div className='relative group'>
+							<h2 className='text-center text-4xl md:text-5xl lg:text-7xl mt-5 text-white cursor-default select-none'>
+								{name}
+							</h2>
+						</div>
+
+						<ul
+							className={`list-none grid grid-cols-2 md:grid-cols-3 lg:grid-cols-[auto,auto,auto,auto,auto] gap-x-10 relative transition duration-200 ease-in scale-0 origin-top w-full ${
+								!toggleTopSite && 'scale-100'
+							}`}>
+							<TopSiteList sites={sites} slices={4} className='mt-10 md:hidden' />
+							<TopSiteList sites={sites} slices={6} className='mt-10 hidden md:block lg:hidden' />
+							<TopSiteList sites={sites} slices={10} className='mt-10 hidden lg:block ' />
+						</ul>
+					</div>
+				)}
+				<button className='absolute bottom-10 right-10 group'>
+					<img src={settings} alt='settings' />
+					<div className='bg-white absolute right-0 bottom-0 rounded-md min-h-[2rem] min-w-[10rem] p-2 hidden group-hover:block'>
+						<ul>
+							<li className='flex items-center' onClick={onEditName}>
+								<img src={edit} className='w-6 h-6 mr-2' alt='edit' />
+								<p className='text-base whitespace-nowrap'>Edit Name</p>
+							</li>
+							<li className='flex items-center mt-3' onClick={toggleTopSites}>
+								<img src={!toggleTopSite ? eyeOff : eye} className='w-6 h-6 mr-2' alt='edit' />
+								<p className='text-base whitespace-nowrap'>
+									{!toggleTopSite ? 'Hide Top Sites' : 'Show Top Sites'}
+								</p>
+							</li>
+						</ul>
+					</div>
+				</button>
+			</div>
+		</>
+	)
+}
+
+export default App
---
+++ src/Form.tsx
@@ -0,0 +1,47 @@
+import { ChangeEvent } from 'react'
+
+export interface FormProps {
+	onSubmit: (e?: any) => void
+	onCancel: () => void
+	inputValue: any
+	inputHandler: (e: ChangeEvent<HTMLInputElement>) => void
+	name: string
+}
+
+export const Form = (props: FormProps) => {
+	const { onSubmit, inputValue, inputHandler, onCancel, name } = props
+
+	return (
+		<div className='flex flex-col items-center z-10 bg-black/50 p-10 rounded-xl'>
+			<label htmlFor='name'>
+				<span className='hidden absolute max-h-0 max-w-0 overflow-hidden'>Hidden Label</span>
+			</label>
+			<input
+				autoFocus
+				autoComplete='off'
+				value={inputValue}
+				onChange={inputHandler}
+				placeholder='Enter Your Name'
+				className='border-b border-green-500 max-w-md focus:outline-none text-center text-2xl lg:text-4xl bg-transparent placeholder:text-gray-50 text-white'
+				type='text'
+				name='userName'
+				id='name'
+			/>
+			<div className='flex mt-5'>
+				{name && (
+					<button
+						onClick={onCancel}
+						className='bg-gray-400 min-w-[100px] p-2 w-full rounded text-black text-base lg:text-lg mr-3'>
+						Cancel
+					</button>
+				)}
+				<button
+					onClick={onSubmit}
+					type='submit'
+					className='bg-green-400 min-w-[100px] p-2 w-full rounded text-black text-base lg:text-lg'>
+					Submit
+				</button>
+			</div>
+		</div>
+	)
+}
